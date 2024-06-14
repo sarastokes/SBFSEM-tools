75a6277699a290c04b5101550844dc28be693fcd
jameskuch <james.kuchenbecker@gmail.com>
2024-06-14 12:19:55 -0700

    added Andreas labelSearch command

---
+++ util/analysis/labelSearch.m
@@ -0,0 +1,20 @@
+%% Script to search for cells by label.
+
+% Specify the volume to search:
+
+disp(['valid options'])
+disp(['     t  = TemporalMonkey2'])
+disp(['     i  = InferiorMonkey'])
+disp(['     nm = NasalMonkey'])
+disp(['     c  = Neitzcped'])
+volume = input('Please enter the volume abbreviation: ','s')
+disp(['You entered: ', volume])
+x = sbfsem.io.OData(volume);
+
+% Specify the label to search. Include false if 
+% not looking for an exact match
+
+label = input('Please enter the label to be searched for: ', 's')
+disp(['You entered: ', label])
+idList = x.idsByLabel(label, false)
+openvar('idList');
