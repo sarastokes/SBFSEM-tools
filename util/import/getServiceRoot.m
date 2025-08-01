52b105e2ae99bd0f5ad612af39919e7ec14b5a1b
jameskuch <james.kuchenbecker@gmail.com>
2025-08-01 13:27:11 -0700

    Update getServiceRoot.m fix character

--- util/import/getServiceRoot.m
+++ util/import/getServiceRoot.m
@@ -24,16 +24,16 @@ function url = getServiceRoot(source)
 
 	source = validateSource(source);
 
-	if strcmp(source, ‘NeitzTemporalMonkey’)
-	    url = ‘http://vpn.codepharm.net/NeitzTemporalMonkey/OData/’;
-	elseif strcmp(source, ‘NeitzInferiorMonkey’)
-	    url = ‘http://vpn.codepharm.net/NeitzInferiorMonkey/OData/’;
-	elseif strcmp(source, ‘NeitzNasalMonkey’)
-	    url = ‘http://vpn.codepharm.net/NeitzNM/OData/’;
-	elseif strcmp(source, ‘NeitzCped’)
-	    url = ‘http://vpn.codepharm.net/NeitzCPED/OData/’;
+	if strcmp(source, 'NeitzTemporalMonkey')
+	    url = 'http://vpn.codepharm.net/NeitzTemporalMonkey/OData/';
+	elseif strcmp(source, 'NeitzInferiorMonkey')
+	    url = 'http://vpn.codepharm.net/NeitzInferiorMonkey/OData/';
+	elseif strcmp(source, 'NeitzNasalMonkey')
+	    url = 'http://vpn.codepharm.net/NeitzNM/OData/';
+	elseif strcmp(source, 'NeitzCped')
+	    url = 'http://vpn.codepharm.net/NeitzCPED/OData/';
 	else
-	    error(‘Unknown source: %s’, source);
+	    error('Unknown source: %s', source);
 	end
 
 	%if strcmp(source, 'NeitzNasalMonkey')        
